d4726b0ecd4931c87f87544c4de4a400bc5b7971
Chee Vue <ctvue@yahoo.com>
2015-05-27 10:10:38 -0700

    Initial Commit

---
+++ MobileMapper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileMapper
+//
+//  Created by Chee Vue on 5/27/15.
+//  Copyright (c) 2015 Chee Vue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobileMapper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileMapper
+//
+//  Created by Chee Vue on 5/27/15.
+//  Copyright (c) 2015 Chee Vue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
