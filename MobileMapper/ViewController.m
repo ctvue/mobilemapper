119fca6c108575a2685117a9eda8d80480a13cdd
Chee Vue <ctvue@yahoo.com>
2015-06-17 14:49:09 -0700

    upload

--- MobileMapper/ViewController.m
+++ MobileMapper/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property MKPointAnnotation *mobileMakersAnnotation; //point that represent the pin
+@property CLLocationManager *manager;
 
 @end
 
@@ -16,12 +20,66 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    //San Diego Airport : 32.7336° N, 117.1897° W
+    double latittude = 41.89373984;
+    double longitude = -87.635392979;
+
+    self.mobileMakersAnnotation = [MKPointAnnotation new];
+    self.mobileMakersAnnotation.coordinate = CLLocationCoordinate2DMake(latittude, longitude);
+    self.mobileMakersAnnotation.title = @"Mobile Makers";
+    [self.mapView addAnnotation:self.mobileMakersAnnotation];
+
+    [self loadAddressString:@"San Diego Airport"];
+    [self loadAddressString:@"593 Eileen St, Brentwood, CA 94513"];
+    [self loadAddressString:@"Sacramento, CA"];
+
+    self.manager = [CLLocationManager new];
+
+    //dont forget to update your plist
+    [self.manager requestWhenInUseAuthorization];
+    self.mapView.showsUserLocation = YES;
+
+
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+
+    if (annotation == mapView.userLocation) {
+        return nil;
+    }
+
+    MKPinAnnotationView *pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"pin"];
+    
+    //view
+    pin.canShowCallout = YES;
+
+    if (annotation == self.mobileMakersAnnotation) {
+        pin.image = [UIImage imageNamed:@"mobilemakers"];
+    }
+
+    pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+
+    return pin;
+}
+
+-(void)loadAddressString:(NSString *)addressString{
+    CLGeocoder *geocoder = [CLGeocoder new];
+    [geocoder geocodeAddressString:addressString completionHandler:^(NSArray *placemarks, NSError *error) {
+        for (CLPlacemark *placemark in placemarks) {
+            MKPointAnnotation *annotation = [MKPointAnnotation new];
+            //add i button
+            annotation.title = placemark.name;
+            annotation.coordinate = placemark.location.coordinate;
+            [self.mapView addAnnotation:annotation];
+        }
+    }];
+}
+
+-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control{
+    MKCoordinateRegion region = MKCoordinateRegionMake(view.annotation.coordinate, MKCoordinateSpanMake(0.01, 0.01));
+    [self.mapView setRegion:region animated:YES];
 }
 
 @end
